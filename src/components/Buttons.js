77b75e1823b542dd82fdffca4e964603da04ab5f
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 18:45:39 +0100

    changed component to flex box

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,9 +8,15 @@ class Buttons extends React.Component {
 
 	render() {
 		const buttonsStyling = {
+			"display": "flex",
+			"flexDirection": "row",
+			"flexWrap": "wrap",
+			"justifyContent": "space-around",
+
 			margin: "0 auto",
 			border: "#FF8A00 solid 1px",
 			borderRadius: "3px",
+			padding: "5px 0 0",
 			width: "90%",
 			height: "280px",
 		};
