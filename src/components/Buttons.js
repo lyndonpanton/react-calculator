eec372fcafecb48a6da275be3cff6cf3a10cd605
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 18:04:51 +0100

    styled component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,12 +2,20 @@ import React from "react";
 
 class Buttons extends React.Component {
 	constructor(props) {
-
+		super(props);
 	}
-	
+
 	render() {
+		const buttonsStyling = {
+			margin: "0 auto",
+			border: "#FF8A00 solid 1px",
+			borderRadius: "3px",
+			width: "90%",
+			height: "280px",
+		};
+
 		return (
-			<div className="buttons">
+			<div className="buttons" style={buttonsStyling}>
 
 			</div>
 		);
