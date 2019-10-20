a69ae2df3a3f8bcb21e60d0efefd280de68b9d34
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 18:49:21 +0100

    styled buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,18 @@ import React from "react";
 const Button = (props) => {
 	const { buttonKey, icon, handleClick } = props;
 
+	const buttonStyling = {
+		borderRadius: "2px",
+		width: "48px",
+		height: "40px",
+		backgroundColor: "#DDDDDD",
+		fontFamily: "Book Antiqua, Palatino, Palatino Linotype, Palatino LT STD, Georgia, serif",
+		fontSize: "1.5em",
+		color: "#222222"
+	};
+
 	return (
-		<button key={buttonKey} onChange={handleClick}>{icon}</button>
+		<button key={buttonKey} style={buttonStyling} onChange={handleClick}>{icon}</button>
 	);
 };
 
