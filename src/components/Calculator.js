02de7d190e08bea35390a76e36ec63ff289ab0dc
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 16:15:33 +0100

    created, exported component

---
+++ src/components/Calculator.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Calculator extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div className="calculator">
+
+			</div>
+		);
+	}
+}
+
+export default Calculator;
