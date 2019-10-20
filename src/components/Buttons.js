e2bd312631468cd0774f5c3c0f52c5b464efa1b8
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 17:54:58 +0100

    created, exported component

---
+++ src/components/Buttons.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Buttons extends React.Component {
+	constructor(props) {
+
+	}
+	
+	render() {
+		return (
+			<div className="buttons">
+
+			</div>
+		);
+	}
+}
+
+export default Buttons;
