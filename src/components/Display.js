eafe43e57508703d17d6558a2461c498f1dc399f
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 16:48:05 +0100

    defined component styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 const Display = (props) => {
+	const displayStyling = {
+		margin: "20px auto",
+		border: "#000000 solid 2px",
+		borderRadius: "3px",
+		width: "90%",
+		height: "50px",
+		backgroundColor: "#225522"
+	};
+
+	const outputStyling = {
+		display: "block",
+		overflowX: "scroll",
+		padding: "0 5px",
+		textAlign: "right",
+		fontFamily: "monospace",
+		fontSize: "3.3em",
+		color: "#EEEEEE"
+	};
+
 	return (
-		<div className="display">
-			<output></output>
+		<div className="display" style={displayStyling}>
+			<output style={outputStyling}></output>
 		</div>
 	);
 };
