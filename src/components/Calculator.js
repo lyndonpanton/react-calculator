e5ffe4952c38d08824c627d6cba66b04a5ab2115
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 18:23:39 +0100

    defined button icons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,8 +6,14 @@ class Calculator extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
-		}
+			icons: ["mc", "m+", "m-", "mr",
+				"c", "/", "x", "<=",
+				"7", "8", "9", "-",
+				"4", "5", "6", "+",
+				"1", "2", "3", "^",
+				"%", "0", ".", "="
+			]
+		};
 	}
 
 	render() {
