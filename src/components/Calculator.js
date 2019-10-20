e2a629a62253c6fe246d821448c052c650eb8242
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 16:27:09 +0100

    define component styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,8 +10,16 @@ class Calculator extends React.Component {
 	}
 
 	render() {
+		const calculatorStyling = {
+			border: "#FF8200 solid 3px",
+			borderRadius: "3px",
+			width: "250px",
+			height: "400px",
+			backgroundColor: "#1A1A1A"
+		};
+
 		return (
-			<div className="calculator">
+			<div className="calculator" style={calculatorStyling}>
 				<Display />
 			</div>
 		);
