98c6d337280cf620f7d8050b198445de8c8e5987
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 16:03:41 +0100

    created, exported component

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<header>
+					<h1>React Calculator</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
