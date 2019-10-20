47145a7dac7c86c807c391c575664198a830b977
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-20 18:29:13 +0100

    removed constructor

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,6 @@ import React from "react";
 import Calculator from "./Calculator";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-
-		};
-	}
-
 	render() {
 		return (
 			<div className="app">
