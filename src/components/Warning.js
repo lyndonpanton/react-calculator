ea2c76ab6186156d16354fb51d669979170609b7
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-21 15:52:47 +0100

    styled component

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -2,9 +2,25 @@ import React from "react";
 
 const Warning = (props) => {
 
+
+	const warningStyling = {
+		margin: "20px auto",
+		border: "#AADDFF solid 2px",
+		borderRadius: "4px",
+		padding: 0,
+		backgroundColor: "#323232",
+		width: "250px",
+		textAlign: "center"
+	};
+
+	const textStyling = {
+		margin: "10px 0 2px",
+		padding: 0,
+	};
+
 	return (
-		<div className="warning">
-			<p>Ok</p>
+		<div className="warning" style={warningStyling}>
+			<p style={textStyling}>Ok</p>
 		</div>
 	);
 };
