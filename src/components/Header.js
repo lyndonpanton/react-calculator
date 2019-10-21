41b7607e030ae00863a1e1f4d2b8cdd8d1862805
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-21 15:22:16 +0100

    styled component elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,19 @@ import React from "react";
 const Header = (props) => {
 	const { name } = props;
 
+	const headerStyling = {
+		backgroundColor: "#800080"
+	};
+
+	const h1Styling = {
+		margin: "0",
+		padding: "8px",
+		textAlign: "center"
+	};
+
 	return (
-		<header>
-			<h1>{name}</h1>
+		<header style={headerStyling}>
+			<h1 style={h1Styling}>{name}</h1>
 		</header>
 	);
 };
