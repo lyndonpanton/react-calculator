583931e16535f2d1ac8f7f3f581bbc0ab0cae71a
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-21 15:14:08 +0100

    imported, rendered header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 import Calculator from "./Calculator";
 
 class App extends React.Component {
@@ -6,7 +7,7 @@ class App extends React.Component {
 		return (
 			<div className="app">
 				<header>
-					<h1>React Calculator</h1>
+					<Header />
 					<Calculator />
 				</header>
 			</div>
